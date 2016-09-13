5d02e45fb6e3e3c56ca2c8399f74f27c544e1b3c
Mario Octavio Jimenez <jimenez303@gmail.com>
2016-09-13 17:22:42 -0600

    base project

---
+++ spaceship_interface.ino
@@ -0,0 +1,30 @@
+int buttonState = 0;
+
+void setup() {
+  pinMode(2, INPUT);
+  pinMode(3, OUTPUT);
+  pinMode(4, OUTPUT);
+  pinMode(5, OUTPUT);
+}
+
+void loop() {
+  buttonState = digitalRead(2);
+  
+  if(buttonState == LOW){
+    // Default state, button is not pressed
+    digitalWrite(3, HIGH);
+    digitalWrite(4, LOW);
+    digitalWrite(5, LOW);
+  }else{
+    // Whenever the button gets pressed
+    digitalWrite(3, LOW);
+    digitalWrite(4, LOW);
+    digitalWrite(5, HIGH);
+    
+    // Lights effect using delay func
+    delay(250);
+    digitalWrite(4, HIGH);
+    digitalWrite(5, LOW);
+    delay(250);   
+  }
+}
